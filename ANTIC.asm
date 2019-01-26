93100a43fb5f9e47eaeb3eda5e64541ff4edb435
Ken Jennings <kj.sumter.42@gmail.com>
2019-01-25 22:54:50 -0500

    Add files via upload

--- ANTIC.asm
+++ ANTIC.asm
@@ -164,7 +164,7 @@ DL_MAP_F = $0F ; 1.5 Color, 320 Pixels x 1 Scan Lines (and GTIA modes), 40 bytes
 ; even (redundantly) the LMS.
 ;-------------------------------------------------------------------------------
 
-.macro mDL_LMS mode,screenMemory
+.macro mDL_LMS  mode,screenMemory
 	.if :0<>2
 		.error "mDL_LMS: 2 arguments required (mode (value of low nybble $2 to $F), screen memory (address)."
 	.endif
@@ -179,3 +179,43 @@ DL_MAP_F = $0F ; 1.5 Color, 320 Pixels x 1 Scan Lines (and GTIA modes), 40 bytes
 	.word :screenMemory   
 .endm
 
+
+;-------------------------------------------------------------------------------
+; 																	DL_LMS 
+;-------------------------------------------------------------------------------
+; mDL_JMP <Address>
+;
+; Declares a JMP DL instruction with the new Display List address in memory.
+;
+;-------------------------------------------------------------------------------
+
+.macro mDL_JMP  screenMemory
+	.if :0<>1
+		.error "mDL_JMP: 1 argument required screen memory (address)."
+	.endif
+
+	; Byte for JMP.  And then the screen memory address.
+	.byte DL_JUMP
+	.word :screenMemory   
+.endm
+
+
+;-------------------------------------------------------------------------------
+; 																	DL_LMS 
+;-------------------------------------------------------------------------------
+; mDL_JVB <Address>
+;
+; Declares a JVB DL instruction (Jump Vertical Blank) with the new 
+; Display List address in memory.
+;
+;-------------------------------------------------------------------------------
+
+.macro mDL_JVB  screenMemory
+	.if :0<>1
+		.error "mDL_JVP: 1 argument required screen memory (address)."
+	.endif
+
+	; Byte for JVB.  And then the screen memory address.
+	.byte DL_JUMP_VB
+	.word :screenMemory   
+.endm
